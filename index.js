a028b2e7a22d065a8fb5b6416e0ebccda10979d5
parth-btb <alokpratap8738@gmail.com>
2025-10-09 15:22:15 +0530

    feat: Add CORS middleware and create test frontend HTML for chat application

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import http from 'http';
 import { Server } from 'socket.io';
+import cors from 'cors';
 import dotenv from 'dotenv';
 import swaggerUi from 'swagger-ui-express';
 import { swaggerSpec } from './config/swagger.js';
@@ -26,6 +27,12 @@ const io = new Server(server, {
 });
 
 // Middleware
+app.use(cors({
+  origin: ['http://localhost:3000', 'http://127.0.0.1:5500', 'http://localhost:5500', 'file://', 'null'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
